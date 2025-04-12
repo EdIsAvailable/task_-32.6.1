6c54f4e5a06eae7793468d61249ee5d185fc106e
EdIsAvailable <nemoed@gmail.com>
2025-04-12 13:02:28 +0000

    Доработан CMake

---
+++ out/build/Configure preset using toolchain file/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/server/task_-32.6.1
+# Build directory: /home/server/task_-32.6.1/out/build/Configure preset using toolchain file
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTests "/home/server/task_-32.6.1/out/build/Configure preset using toolchain file/task_32.6.1")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/server/task_-32.6.1/CMakeLists.txt;33;add_test;/home/server/task_-32.6.1/CMakeLists.txt;0;")
