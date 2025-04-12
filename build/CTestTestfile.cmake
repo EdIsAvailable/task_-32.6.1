494e03d68939ff78b2d58cca83eefd372b35c5c7
EdIsAvailable <nemoed@gmail.com>
2025-04-12 19:08:10 +0000

    Добавил в игнор build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/server/task_-32.6.1
-# Build directory: /home/server/task_-32.6.1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
